4c69d996c76adb5b714ffd2854e7da370d4f9ec2
Nicolas W <nwadoux@myges.fr>
2024-02-19 20:51:26 +0100

    add test for FIRST cards

--- back/test/unit/quizzService.test.js
+++ back/test/unit/quizzService.test.js
@@ -2,6 +2,7 @@ const storageConnector = new (require('../../src/application/connectors/StorageC
 const Card = require('../../src/domain/entities/Card');
 const {QuizzService: QuizzServiceClass} = require("../../src/domain/services/QuizzService");
 const QuizzService = new QuizzServiceClass({ storageConnector });
+const Category = require('../../src/domain/entities/Category');
 
 const generateCards = (amount) => {
     const cards = [];
@@ -41,4 +42,28 @@ describe('Quizz Service test', () => {
         expect(cards).toHaveLength(2);
     });
 
+    it('should return cards never answered', () => {
+        const today = new Date('2021-01-01');
+
+        jest.spyOn(storageConnector, "getCards").mockReturnValue([
+            new Card('1', 'How are you ?', 'I am fine'),
+        ]);
+
+        const cards = QuizzService.getQuizz(today);
+        expect(cards).toBeInstanceOf(Array);
+        expect(cards).toHaveLength(1);
+    });
+
+    it('should return cards of category FIRST', () => {
+        const yesterday = new Date('2021-01-01');
+        const today = new Date('2021-01-02');
+
+        jest.spyOn(storageConnector, "getCards").mockReturnValue([
+            new Card('1', 'How are you ?', 'I am fine', null, Category.FIRST, yesterday),
+        ]);
+
+        const cards = QuizzService.getQuizz(today);
+        expect(cards).toBeInstanceOf(Array);
+        expect(cards).toHaveLength(1);
+    });
 });
