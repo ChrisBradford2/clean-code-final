84e01052a25690f5d9e80b12b246a6848ff024b0
Nicolas W <nwadoux@myges.fr>
2024-02-19 21:05:34 +0100

    test case when card expected or not expected

--- back/test/unit/quizzService.test.js
+++ back/test/unit/quizzService.test.js
@@ -79,4 +79,31 @@ describe('Quizz Service test', () => {
         expect(cards).toBeInstanceOf(Array);
         expect(cards).toHaveLength(1);
     });
+
+    it('should return cards of category SECOND OR FIRST', () => {
+        const today = new Date('2021-01-02');
+        const twoDaysBefore = new Date('2020-12-31');
+        const fiveDaysBefore = new Date('2020-12-28');
+        const yesterday = new Date('2021-01-01');
+
+        const cardsExpected = [
+            new Card('1', 'How are you ?', 'I am fine', null, Category.SECOND, twoDaysBefore),
+            new Card('1', 'How are you ?', 'I am fine', null, Category.SECOND, fiveDaysBefore),
+            new Card('2', 'How are you ?', 'I am fine', null, Category.FIRST, yesterday),
+            new Card('2', 'How are you ?', 'I am fine', null, Category.FIRST, twoDaysBefore),
+        ];
+
+        const cardsNotExpected = [
+            new Card('3', 'How are you ?', 'I am fine', null, Category.FIRST, today),
+            new Card('3', 'How are you ?', 'I am fine', null, Category.SECOND, today),
+            new Card('3', 'How are you ?', 'I am fine', null, Category.SECOND, yesterday),
+        ];
+
+        jest.spyOn(storageConnector, "getCards").mockReturnValue([...cardsExpected, ...cardsNotExpected]);
+
+        const cards = QuizzService.getQuizz(today);
+        expect(cards).toBeInstanceOf(Array);
+        expect(cards).toHaveLength(4);
+        expect(cards).toEqual(cardsExpected);
+    });
 });
